6e91fd9b8e7936ce5495b194009b010b135b8a8d
Mark Edward M. Gonzales <gonzales.markedward@gmail.com>
2023-01-11 16:23:01 +0800

    Complete scripts for EDA

--- experiments/EDAUtil.py
+++ experiments/EDAUtil.py
@@ -1,5 +1,16 @@
+import os
+
 class EDAUtil(object):
-    def __init__():
+    def __init__(self):
         pass
     
-    
+    def get_rbps(self, directory):
+        rbps = []
+        for file in os.listdir(f'{directory}'):
+            with open(f'{directory}/{file}') as embeddings:
+                for embedding in embeddings:
+                    rbp_id = embedding.split(',')[0]
+                    if rbp_id != 'ID':
+                        rbps.append(rbp_id)
+                
+        return rbps
