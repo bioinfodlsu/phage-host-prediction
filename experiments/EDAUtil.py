5ef997be3036e99b6cfb720c9c2b95cfef7140bf
Mark Edward M. Gonzales <gonzales.markedward@gmail.com>
2023-01-14 19:12:18 +0800

    Document the methods for EDA

--- experiments/EDAUtil.py
+++ experiments/EDAUtil.py
@@ -1,10 +1,29 @@
+"""
+=========================================================================
+This script contains the utility functions for exploratory data analysis.
+@author    Mark Edward M. Gonzales
+=========================================================================
+"""
+
 import os
 
 class EDAUtil(object):
     def __init__(self):
+        """
+        Constructor
+        """
         pass
     
     def get_rbps(self, directory):
+        """
+        Get the protein IDs in the RBP embeddings files stored in the given directory
+        
+        Parameters:
+        - directory: File path of the directory where the RBP embeddings files are saved
+        
+        Returns:
+        - List of protein IDs in the RBP embeddings files stored in the directory
+        """
         rbps = []
         for file in os.listdir(f'{directory}'):
             with open(f'{directory}/{file}') as embeddings:
